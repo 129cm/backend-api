756b731a18837e8f4423791df747bde207f8888a
kim-youngchul <kmosh1212@naver.com>
2024-05-20 16:38:45 +0900

    feat: 테스트 컨테이너 적용

--- src/test/java/com/d129cm/backendapi/BackendApiApplicationTests.java
+++ src/test/java/com/d129cm/backendapi/BackendApiApplicationTests.java
@@ -1,9 +1,12 @@
 package com.d129cm.backendapi;
 
+import com.d129cm.backendapi.config.InitializeTestContainers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
 
 @SpringBootTest
+@ImportTestcontainers(InitializeTestContainers.class)
 class BackendApiApplicationTests {
 
     @Test
