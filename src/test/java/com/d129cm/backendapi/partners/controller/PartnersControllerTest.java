25a0498d3258bbe504922f1ddc11ccd859a068e9
kim-youngchul <kmosh1212@naver.com>
2024-05-25 01:51:38 +0900

    style: unused import 삭제

--- src/test/java/com/d129cm/backendapi/partners/controller/PartnersControllerTest.java
+++ src/test/java/com/d129cm/backendapi/partners/controller/PartnersControllerTest.java
@@ -15,8 +15,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import java.nio.charset.StandardCharsets;
-
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
