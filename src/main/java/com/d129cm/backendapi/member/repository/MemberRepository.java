a6b316b06d09c9b7ed3c61106a246e390ad6d872
kuma0112 <lsj23871@gmail.com>
2024-05-25 02:56:27 +0900

    feat: Address, Member Repository 생성

---
+++ src/main/java/com/d129cm/backendapi/member/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.d129cm.backendapi.member.repository;
+
+import com.d129cm.backendapi.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
