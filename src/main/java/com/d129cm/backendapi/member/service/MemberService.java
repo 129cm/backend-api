940b5c7ed0fc9a706bff53eb7c120436eb2912d5
kuma0112 <lsj23871@gmail.com>
2024-05-26 14:32:28 +0900

    feat: member 회원가입 서비스 작성, 테스트 작성

---
+++ src/main/java/com/d129cm/backendapi/member/service/MemberService.java
@@ -0,0 +1,28 @@
+package com.d129cm.backendapi.member.service;
+
+import com.d129cm.backendapi.common.exception.BaseException;
+import com.d129cm.backendapi.member.domain.Member;
+import com.d129cm.backendapi.member.dto.MemberSignupRequest;
+import com.d129cm.backendapi.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public void saveMember(MemberSignupRequest request) {
+        if (memberRepository.existsByEmail(request.email())) {
+            throw new BaseException("이미 가입된 이메일입니다.", HttpStatus.CONFLICT.value());
+        }
+
+        Member newMember = request.toMemberEntity();
+
+        memberRepository.save(newMember);
+    }
+}
---
+++ src/test/java/com/d129cm/backendapi/member/service/MemberServiceTest.java
@@ -0,0 +1,66 @@
+package com.d129cm.backendapi.member.service;
+
+import com.d129cm.backendapi.common.domain.Address;
+import com.d129cm.backendapi.common.exception.BaseException;
+import com.d129cm.backendapi.member.domain.Member;
+import com.d129cm.backendapi.member.dto.MemberSignupRequest;
+import com.d129cm.backendapi.member.repository.MemberRepository;
+import com.d129cm.backendapi.partners.domain.Partners;
+import com.d129cm.backendapi.partners.dto.PartnersSignupRequest;
+import com.d129cm.backendapi.partners.repository.PartnersRepository;
+import com.d129cm.backendapi.partners.service.PartnersService;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.Assert.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class MemberServiceTest {
+
+    @InjectMocks
+    private MemberService memberService;
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Nested
+    class saveMember {
+        @Test
+        void 성공반환_멤버_저장() {
+            // given
+            MemberSignupRequest request = new MemberSignupRequest(
+                    "test@naver.com", "asdf1234!", "이름", mock(Address.class)
+            );
+
+            when(memberRepository.save(any(Member.class))).thenReturn(mock(Member.class));
+
+            // when
+            memberService.saveMember(request);
+
+            // then
+            verify(memberRepository).save(any(Member.class));
+        }
+
+        @Test
+        void 예외반환_중복된_멤버() {
+            // given
+            MemberSignupRequest request = new MemberSignupRequest(
+                    "test@naver.com", "asdf1234!", "이름", mock(Address.class)
+            );
+
+            // when
+            when(memberRepository.existsByEmail(request.email())).thenReturn(true);
+
+            // then
+            assertThrows(BaseException.class, () -> memberService.saveMember(request));
+        }
+    }
+
+}
