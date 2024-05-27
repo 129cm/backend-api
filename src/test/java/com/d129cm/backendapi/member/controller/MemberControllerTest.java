76c6510b5866b402b03526437f41d6058daa37a3
kuma0112 <lsj23871@gmail.com>
2024-05-27 12:29:36 +0900

    feat: member 회원가입 컨트롤러 기능 작성, 테스트 작성

---
+++ src/test/java/com/d129cm/backendapi/member/controller/MemberControllerTest.java
@@ -0,0 +1,58 @@
+package com.d129cm.backendapi.member.controller;
+
+import com.d129cm.backendapi.common.domain.Address;
+import com.d129cm.backendapi.member.dto.MemberSignupRequest;
+import com.d129cm.backendapi.member.service.MemberService;
+import com.d129cm.backendapi.security.CustomSecurityConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MemberController.class)
+@Import(CustomSecurityConfig.class)
+@SuppressWarnings("NonAsciiCharacters")
+public class MemberControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private MemberService memberService;
+
+    @Nested
+    class signup {
+        @Test
+        @WithMockUser(username = "testUser", roles = {"USER"})
+        void 성공반환_멤버_회원가입_요청() throws Exception {
+            // given
+            MemberSignupRequest request = new MemberSignupRequest("test@naver.com", "asdf1234!", "이름", mock(Address.class));
+            doNothing().when(memberService).saveMember(request);
+
+            // when
+            ResultActions result = mockMvc.perform(post("/members/signup")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .characterEncoding("UTF-8")
+                    .content(new ObjectMapper().writeValueAsString(request)));
+
+            // then
+            result.andExpect(status().isOk())
+                    .andExpect(content().json("{\"status\":200, \"message\":\"성공\"}"));
+        }
+    }
+}
