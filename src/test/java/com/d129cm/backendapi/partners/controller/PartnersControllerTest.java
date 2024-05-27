93e342f9b5e15f7a8f894f6988429702a24b740b
kim-youngchul <kmosh1212@naver.com>
2024-05-27 15:25:36 +0900

    feat: conflictException 적용

--- src/test/java/com/d129cm/backendapi/partners/controller/PartnersControllerTest.java
+++ src/test/java/com/d129cm/backendapi/partners/controller/PartnersControllerTest.java
@@ -1,5 +1,7 @@
 package com.d129cm.backendapi.partners.controller;
 
+import com.d129cm.backendapi.common.dto.CommonResponse;
+import com.d129cm.backendapi.common.exception.ConflictException;
 import com.d129cm.backendapi.partners.dto.PartnersSignupRequest;
 import com.d129cm.backendapi.partners.service.PartnersService;
 import com.d129cm.backendapi.security.CustomSecurityConfig;
@@ -7,7 +9,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
@@ -15,12 +16,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.nio.charset.StandardCharsets;
+
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@AutoConfigureMockMvc
 @WebMvcTest(PartnersController.class)
 @Import(CustomSecurityConfig.class)
 @SuppressWarnings("NonAsciiCharacters")
@@ -43,12 +46,34 @@ public class PartnersControllerTest {
             // when
             ResultActions result = mockMvc.perform(post("/partners/signup")
                     .contentType(MediaType.APPLICATION_JSON)
-                    .characterEncoding("UTF-8")
+                    .characterEncoding(StandardCharsets.UTF_8)
                     .content(new ObjectMapper().writeValueAsString(request)));
 
             // then
             result.andExpect(status().isOk())
                     .andExpect(content().json("{\"status\":200, \"message\":\"성공\"}"));
+        }
+
+        @Test
+        void status409_이메일_중복() throws Exception {
+            // given
+            PartnersSignupRequest request = new PartnersSignupRequest("email@naver.com", "password", "123-12-12312");
+            ConflictException e = new ConflictException("email", request.email());
+
+            doThrow(e).when(partnersService).savePartners(request);
+
+            CommonResponse<Void> expectedResponse = CommonResponse.failure(e.getStatus(), e.getMessage());
+            String expectedResponseJson = new ObjectMapper().writeValueAsString(expectedResponse);
+
+            // when
+            ResultActions result = mockMvc.perform(post("/partners/signup")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .characterEncoding(StandardCharsets.UTF_8)
+                    .content(new ObjectMapper().writeValueAsString(request)));
+
+            // then
+            result.andExpect(status().isConflict())
+                    .andExpect(content().json(expectedResponseJson));
         }
     }
 }
