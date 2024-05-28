e446c07994ba4d8363346ad9c0d6bcda13cedcec
kim-youngchul <kmosh1212@naver.com>
2024-05-28 18:43:07 +0900

    feat: partners email find 추가, 테스트 작성

--- src/main/java/com/d129cm/backendapi/partners/repository/PartnersRepository.java
+++ src/main/java/com/d129cm/backendapi/partners/repository/PartnersRepository.java
@@ -3,6 +3,9 @@ package com.d129cm.backendapi.partners.repository;
 import com.d129cm.backendapi.partners.domain.Partners;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PartnersRepository extends JpaRepository<Partners, Long>{
     boolean existsByEmail(String email);
+    Optional<Partners> findByEmail(String email);
 }
--- src/test/java/com/d129cm/backendapi/partners/repository/PartnersRepositoryTest.java
+++ src/test/java/com/d129cm/backendapi/partners/repository/PartnersRepositoryTest.java
@@ -3,11 +3,15 @@ package com.d129cm.backendapi.partners.repository;
 import com.d129cm.backendapi.config.InitializeTestContainers;
 import com.d129cm.backendapi.partners.domain.Partners;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.testcontainers.context.ImportTestcontainers;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,6 +22,19 @@ public class PartnersRepositoryTest {
 
     @Autowired
     private PartnersRepository partnersRepository;
+
+    @BeforeEach
+    void setUp() {
+        partnersRepository.deleteAll();
+
+        Partners partners = Partners.builder()
+                .email("test@naver.com")
+                .password("asdf1234!")
+                .businessNumber("111-11-11111")
+                .build();
+
+        partnersRepository.save(partners);
+    }
 
     @Test
     void 성공_파트너_저장() {
@@ -39,5 +56,44 @@ public class PartnersRepositoryTest {
                 () -> assertThat(savedPartners.getPassword()).isEqualTo(partners.getPassword()),
                 () -> assertThat(savedPartners.getBusinessNumber()).isEqualTo(partners.getBusinessNumber())
         );
+    }
+
+    @Nested
+    class existByEmail {
+        @Test
+        void true_반환_파트너스_존재유무_확인() {
+            //given
+            Partners existPartners = Partners.builder()
+                    .email("test@naver.com")
+                    .password("asdf1234!")
+                    .businessNumber("111-11-11111")
+                    .build();
+
+            // when
+            boolean result = partnersRepository.existsByEmail(existPartners.getEmail());
+
+            // then
+            assertThat(result).isTrue();
+        }
+    }
+
+    @Nested
+    class findByEmail {
+        @Test
+        void 유효한_파트너스_반환_email로_파트너스_찾기() {
+            // given
+            Partners existPartners = Partners.builder()
+                    .email("test@naver.com")
+                    .password("asdf1234!")
+                    .businessNumber("111-11-11111")
+                    .build();
+
+            // when
+            Optional<Partners> partners = partnersRepository.findByEmail(existPartners.getEmail());
+
+            // then
+            assertThat(partners).isPresent();
+            assertThat(partners.get().getEmail()).isEqualTo(existPartners.getEmail());
+        }
     }
 }
