3baa52063eb3b8ac9f3e22b8849f7dac2c2a66da
kuma0112 <lsj23871@gmail.com>
2024-05-29 23:41:27 +0900

    feat: Member 시큐리티 필터 생성

---
+++ src/main/java/com/d129cm/backendapi/auth/filter/JwtAuthorizationFilter.java
@@ -0,0 +1,62 @@
+package com.d129cm.backendapi.auth.filter;
+
+import com.d129cm.backendapi.auth.domain.Role;
+import com.d129cm.backendapi.auth.service.MemberDetailsService;
+import com.d129cm.backendapi.auth.utils.CookieUtils;
+import com.d129cm.backendapi.auth.utils.JwtProvider;
+import com.d129cm.backendapi.common.dto.CommonResponse;
+import com.d129cm.backendapi.global.utils.ServletResponseUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+     private final JwtProvider jwtProvider;
+     private final MemberDetailsService memberService;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            CookieUtils.getCookie(req, HttpHeaders.AUTHORIZATION).ifPresent(
+                    cookie -> {
+                        String tokenValue = cookie.getValue();
+                        tokenValue = jwtProvider.removeBearerPrefix(tokenValue);
+                        jwtProvider.validateToken(tokenValue);
+                        String username = jwtProvider.getSubjectFromToken(tokenValue);
+                        Role role = jwtProvider.getRoleFromToken(tokenValue);
+                        setAuthentication(username, role);
+                    }
+            );
+        } catch (Exception e) {
+            SecurityContextHolder.clearContext();
+
+            CommonResponse<?> responseDto = CommonResponse.failure(HttpStatus.UNAUTHORIZED, "토큰이 유효하지 않습니다.");
+            ServletResponseUtil.servletResponse(res, responseDto);
+        }
+        filterChain.doFilter(req, res);
+    }
+
+    private void setAuthentication(String username, Role role) {
+        Authentication authentication = createAuthentication(username, role);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private Authentication createAuthentication(String username, Role role) {
+        UserDetails userDetails = memberService.loadUserByUsername(username);
+        return new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
+    }
+}
---
+++ src/main/java/com/d129cm/backendapi/auth/filter/MemberAuthenticationFilter.java
@@ -0,0 +1,78 @@
+package com.d129cm.backendapi.auth.filter;
+
+import com.d129cm.backendapi.auth.domain.Role;
+import com.d129cm.backendapi.auth.dto.LoginRequest;
+import com.d129cm.backendapi.auth.dto.LoginResponse;
+import com.d129cm.backendapi.auth.utils.CookieUtils;
+import com.d129cm.backendapi.auth.utils.JwtProvider;
+import com.d129cm.backendapi.common.dto.CommonResponse;
+import com.d129cm.backendapi.global.utils.ServletResponseUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class MemberAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final JwtProvider jwtProvider;
+
+    public MemberAuthenticationFilter(JwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+        setFilterProcessesUrl("/members/login");
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        try {
+            LoginRequest requestDto = new ObjectMapper().readValue(request.getInputStream(), LoginRequest.class);
+
+            return getAuthenticationManager().authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            requestDto.email(),
+                            requestDto.password()
+                    )
+            );
+        } catch (IOException e) {
+            throw new AuthenticationServiceException("로그인 시도 중 오류가 발생했습니다.", e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        UserDetails userDetails = (UserDetails) authResult.getPrincipal();
+        String username = userDetails.getUsername();
+        Role role = userDetails.getAuthorities().stream().findFirst().map(Role.class::cast).orElseThrow();
+
+        String token = jwtProvider.createToken(username, role);
+
+        String encodedToken = URLEncoder.encode(token, StandardCharsets.UTF_8).replace("+", "%20");
+        Cookie cookie = CookieUtils.createCookie(HttpHeaders.AUTHORIZATION, encodedToken, jwtProvider.JWT_TOKEN_EXPIRATION_TIME);
+        response.addCookie(cookie);
+
+        CommonResponse<?> successResponse = CommonResponse.success(HttpStatus.OK, new LoginResponse(username));
+
+        ServletResponseUtil.servletResponse(response, successResponse);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        CommonResponse<?> errorResponse = CommonResponse.failure(HttpStatus.UNAUTHORIZED, "인증 과정 중 오류 발생");
+
+        ServletResponseUtil.servletResponse(response, errorResponse);
+    }
+}
