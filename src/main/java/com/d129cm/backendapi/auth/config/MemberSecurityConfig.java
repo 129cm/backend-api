69495a2524d51e8dbe907c1a665c71a87015eeaf
kuma0112 <lsj23871@gmail.com>
2024-05-29 23:41:10 +0900

    feat: Member 시큐리티 config 생성

---
+++ src/main/java/com/d129cm/backendapi/auth/config/MemberSecurityConfig.java
@@ -0,0 +1,99 @@
+package com.d129cm.backendapi.auth.config;
+
+import com.d129cm.backendapi.auth.filter.JwtAuthorizationFilter;
+import com.d129cm.backendapi.auth.filter.MemberAuthenticationFilter;
+import com.d129cm.backendapi.auth.service.MemberDetailsService;
+import com.d129cm.backendapi.auth.utils.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class MemberSecurityConfig {
+
+    private final JwtProvider jwtProvider;
+    private final MemberDetailsService memberService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider memberProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(memberService);
+        return provider;
+    }
+    @Bean
+    public AuthenticationManager memberAuthenticationManager() {
+        return new ProviderManager(memberProvider());
+    }
+
+
+    @Bean
+    public MemberAuthenticationFilter memberAuthenticationFilter() {
+        MemberAuthenticationFilter memberAuthenticationFilter = new MemberAuthenticationFilter(jwtProvider);
+        memberAuthenticationFilter.setAuthenticationManager(memberAuthenticationManager());
+        return memberAuthenticationFilter;
+    }
+
+    @Bean
+    public JwtAuthorizationFilter jwtAuthorizationFilter() {
+        return new JwtAuthorizationFilter(jwtProvider, memberService);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedHeaders(List.of("*"));
+//        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://eroom-challenge.com", "https://www.eroom-challenge.com"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setMaxAge(60L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean(name = "memberSecurityFilterChain")
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(s ->  s.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.authorizeHttpRequests(common -> common
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll());
+
+        http.authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll());
+
+        http.formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
