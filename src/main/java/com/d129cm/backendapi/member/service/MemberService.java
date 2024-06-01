807436d2b8bd6992a951e123aba762246fa29999
kuma0112 <lsj23871@gmail.com>
2024-06-02 01:08:53 +0900

    fix: 500 -> 409 반환하도록 수정

--- src/main/java/com/d129cm/backendapi/member/service/MemberService.java
+++ src/main/java/com/d129cm/backendapi/member/service/MemberService.java
@@ -1,12 +1,11 @@
 package com.d129cm.backendapi.member.service;
 
 import com.d129cm.backendapi.common.domain.Password;
-import com.d129cm.backendapi.common.exception.BaseException;
+import com.d129cm.backendapi.common.exception.ConflictException;
 import com.d129cm.backendapi.member.domain.Member;
 import com.d129cm.backendapi.member.dto.MemberSignupRequest;
 import com.d129cm.backendapi.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,7 +21,7 @@ public class MemberService {
     public void saveMember(MemberSignupRequest request) {
         Password newPassword = Password.of(request.password(), passwordEncoder);
         if (memberRepository.existsByEmail(request.email())) {
-            throw new BaseException("이미 가입된 이메일입니다.", HttpStatus.CONFLICT);
+            throw new ConflictException("email", request.email());
         }
 
         Member newMember = Member.builder()
