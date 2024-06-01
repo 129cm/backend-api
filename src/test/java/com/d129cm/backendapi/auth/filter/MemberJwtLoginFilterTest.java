a29205680e90e327e5bd915cd83d4d7efadfb5b2
kuma0112 <lsj23871@gmail.com>
2024-06-02 01:43:20 +0900

    feat: member 필터 테스트 생성

---
+++ src/test/java/com/d129cm/backendapi/auth/filter/MemberJwtLoginFilterTest.java
@@ -0,0 +1,93 @@
+package com.d129cm.backendapi.auth.filter;
+
+import com.d129cm.backendapi.auth.config.MemberSecurityConfig;
+import com.d129cm.backendapi.auth.domain.MemberDetails;
+import com.d129cm.backendapi.auth.dto.LoginRequest;
+import com.d129cm.backendapi.auth.service.MemberDetailsService;
+import com.d129cm.backendapi.auth.utils.JwtProvider;
+import com.d129cm.backendapi.common.domain.Address;
+import com.d129cm.backendapi.common.domain.Password;
+import com.d129cm.backendapi.member.domain.Member;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MemberJwtLoginFilter.class)
+@ContextConfiguration(classes = {MemberSecurityConfig.class})
+class MemberJwtLoginFilterTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private JwtProvider jwtProvider;
+
+    @MockBean
+    private AuthenticationManager authenticationManager;
+
+    @MockBean
+    private MemberDetailsService memberDetailsService;
+
+    @MockBean
+    private PasswordEncoder passwordEncoder;
+
+    @Nested
+    class Authenticate {
+        @Test
+        void 성공200반환_멤버_로그인_성공() throws Exception {
+            // given
+            LoginRequest loginRequest = new LoginRequest("test@naver.com", "Asdf1234!");
+            String loginRequestJson = new ObjectMapper().writeValueAsString(loginRequest);
+
+            Member member = new Member("test@naver.com", Password.of("Asdf1234", passwordEncoder), "이름", mock(Address.class));
+            MemberDetails memberDetails = new MemberDetails(member);
+
+            Authentication authResult = new UsernamePasswordAuthenticationToken(memberDetails, null, memberDetails.getAuthorities());
+            when(memberDetailsService.loadUserByUsername("test@naver.com")).thenReturn(memberDetails);
+            when(authenticationManager.authenticate(any())).thenReturn(authResult);
+            when(jwtProvider.createToken(any(), any())).thenReturn("mocked-jwt-token");
+
+            // when & then
+            mockMvc.perform(post("/members/login")
+                            .contentType("application/json")
+                            .content(loginRequestJson))
+                     .andExpect(status().isOk());
+        }
+
+        @Test
+        void 에러403반환_멤버_로그인_실패() throws Exception {
+            // given
+            LoginRequest loginRequest = new LoginRequest("test@naver.com", "wrongPassword");
+            String loginRequestJson = new ObjectMapper().writeValueAsString(loginRequest);
+
+            when(authenticationManager.authenticate(any()))
+                    .thenThrow(new AuthenticationServiceException("Invalid credentials"));
+
+            // when & then
+            mockMvc.perform(post("/members/login")
+                            .contentType("application/json")
+                            .content(loginRequestJson))
+                    .andExpect(status().isUnauthorized());
+        }
+    }
+}
