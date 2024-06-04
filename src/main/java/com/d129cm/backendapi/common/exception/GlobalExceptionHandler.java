b404da042bc5464666a9461c82a824568da3c5a8
kuma0112 <lsj23871@gmail.com>
2024-06-04 16:00:43 +0900

    refactor: GlobalExceptionHandler 중복 로직 제거
    BaseException을 상속받는 모든 예외는 handleBaseExceptions 메서드에서 처리.

--- src/main/java/com/d129cm/backendapi/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/d129cm/backendapi/common/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.d129cm.backendapi.common.dto.CommonResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -30,45 +31,26 @@ public class GlobalExceptionHandler {
                 .map(error -> String.format("%s: %s", error.getField(), error.getDefaultMessage()))
                 .collect(Collectors.joining(", "));
 
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(CommonResponse.failure(HttpStatus.BAD_REQUEST, errorMessage));
-    }
-
-    @ExceptionHandler(ConflictException.class)
-    public final ResponseEntity<?> handleConflictException(ConflictException e, HttpServletRequest request) {
-        log.warn("source = {} {}, message = {}",
-                request.getMethod(), request.getRequestURI(), e.getMessage());
-        return ResponseEntity
-                .status(e.getStatus())
-                .body(CommonResponse.failure(e.getStatus(), e.getMessage()));
-    }
-
-    @ExceptionHandler(AuthenticationException.class)
-    public final ResponseEntity<?> handleAuthenticationException(AuthenticationException e, HttpServletRequest request) {
-        log.warn("source = {} {}, message = {}",
-                request.getMethod(), request.getRequestURI(), e.getMessage());
-        return ResponseEntity
-                .status(e.getStatus())
-                .body(CommonResponse.failure(e.getStatus(), e.getMessage()));
+        return buildResponseEntity(HttpStatus.BAD_REQUEST, errorMessage);
     }
 
     @ExceptionHandler(BaseException.class)
-    public final ResponseEntity<?> handleFinderException(BaseException e, HttpServletRequest request) {
+    public final ResponseEntity<CommonResponse<Void>> handle129cmException(BaseException e, HttpServletRequest request) {
         log.warn("source = {} {}, message = {}",
                 request.getMethod(), request.getRequestURI(), e.getMessage());
-        return ResponseEntity
-                .status(e.getStatus())
-                .body(CommonResponse.failure(e.getStatus(), e.getMessage()));
+        return buildResponseEntity(e.getStatus(), e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<CommonResponse<Void>> handleDefaultExceptions(Exception e, HttpServletRequest request) {
         log.error("source = {} {}, message = {}",
                 request.getMethod(), request.getRequestURI(), e.getMessage(), e);
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(CommonResponse.failure(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR"));
+        return buildResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR");
     }
 
+    private static ResponseEntity<CommonResponse<Void>> buildResponseEntity(HttpStatusCode statusCode, String message) {
+        return ResponseEntity
+                .status(statusCode)
+                .body(CommonResponse.failure(statusCode, message));
+    }
 }
