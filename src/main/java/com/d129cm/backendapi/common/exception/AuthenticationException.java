8b33f02949c2f66dc2a9f4ac0f2b737f7d3f6242
kuma0112 <lsj23871@gmail.com>
2024-06-04 16:03:56 +0900

    refactor: AuthenticationException + ConflictException 리팩토링
    정적 팩토리 메서드 사용 + 예외 메시지는 파라미터로 -> 다양한 오류 처리

--- src/main/java/com/d129cm/backendapi/common/exception/AuthenticationException.java
+++ src/main/java/com/d129cm/backendapi/common/exception/AuthenticationException.java
@@ -3,7 +3,14 @@ package com.d129cm.backendapi.common.exception;
 import org.springframework.http.HttpStatus;
 
 public class AuthenticationException extends BaseException {
-    public AuthenticationException(String token) {
-        super(String.format("토큰 : '%s'는 유효하지 않습니다.", token), HttpStatus.UNAUTHORIZED);
+    private static final String UNAUTHENTICATED_TOKEN = "토큰 '%s'는 유효하지 않습니다.";
+
+    public AuthenticationException(String message) {
+        super(message, HttpStatus.UNAUTHORIZED);
     }
+
+    public static AuthenticationException unauthenticatedToken(String token) {
+        return new AuthenticationException(String.format(UNAUTHENTICATED_TOKEN, token));
+    }
+
 }
