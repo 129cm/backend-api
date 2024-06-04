f8427577a4f1c6de1557a2061259a53e8122f394
kim-youngchul <kmosh1212@naver.com>
2024-06-04 11:49:58 +0900

    feat: PartnersMyPage 뷰 작성

--- src/main/java/com/d129cm/backendapi/partners/controller/PartnersController.java
+++ src/main/java/com/d129cm/backendapi/partners/controller/PartnersController.java
@@ -1,11 +1,15 @@
 package com.d129cm.backendapi.partners.controller;
 
 import com.d129cm.backendapi.common.dto.CommonResponse;
+import com.d129cm.backendapi.partners.domain.Partners;
+import com.d129cm.backendapi.partners.dto.PartnersMyPageResponse;
 import com.d129cm.backendapi.partners.dto.PartnersSignupRequest;
 import com.d129cm.backendapi.partners.service.PartnersService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +25,13 @@ public class PartnersController {
         partnersService.savePartners(request);
 
         return ResponseEntity.ok().body(CommonResponse.success());
+    }
+
+    @GetMapping("/partners")
+    public ResponseEntity<CommonResponse<PartnersMyPageResponse>> getPartnersMyPage(
+            @AuthenticationPrincipal(expression = "partners") Partners partners) {
+        PartnersMyPageResponse response = partnersService.getPartnersMyPage(partners);
+
+        return ResponseEntity.ok().body(CommonResponse.success(response));
     }
 }
