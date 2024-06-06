7412a2dfda8c6626891c8d6774df1f6cf361b4e7
kuma0112 <lsj23871@gmail.com>
2024-06-06 21:12:19 +0900

    feat: itemRepository 브랜드id 조회 생성

--- src/main/java/com/d129cm/backendapi/item/repository/ItemRepository.java
+++ src/main/java/com/d129cm/backendapi/item/repository/ItemRepository.java
@@ -3,5 +3,8 @@ package com.d129cm.backendapi.item.repository;
 import com.d129cm.backendapi.item.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findAllByBrandId(Long brandId);
 }
