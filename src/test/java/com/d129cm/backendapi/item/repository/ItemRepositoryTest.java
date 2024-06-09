6c7af92f6314b9494255454c711198364a9520ed
kuma0112 <lsj23871@gmail.com>
2024-06-10 00:46:25 +0900

    test: ItemRepository 테스트 생성

---
+++ src/test/java/com/d129cm/backendapi/item/repository/ItemRepositoryTest.java
@@ -0,0 +1,57 @@
+package com.d129cm.backendapi.item.repository;
+
+import com.d129cm.backendapi.common.domain.Address;
+import com.d129cm.backendapi.common.domain.Password;
+import com.d129cm.backendapi.config.InitializeTestContainers;
+import com.d129cm.backendapi.item.domain.Item;
+import com.d129cm.backendapi.member.domain.Member;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ImportTestcontainers(InitializeTestContainers.class)
+public class ItemRepositoryTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Nested
+    class create {
+        @Test
+        void 성공_아이템_저장() {
+            //given
+            Item item = Item.builder()
+                    .name("상품 이름")
+                    .quantity(100)
+                    .price(1000)
+                    .image("상품 이미지")
+                    .description("상품 설명")
+                    .build();
+
+            // when
+            Item savedItem = itemRepository.save(item);
+
+            // then
+            Assertions.assertAll(
+                    () -> assertThat(savedItem).isNotNull(),
+                    () -> assertThat(savedItem.getId()).isEqualTo(item.getId()),
+                    () -> assertThat(savedItem.getName()).isEqualTo(item.getName()),
+                    () -> assertThat(savedItem.getQuantity()).isEqualTo(item.getQuantity()),
+                    () -> assertThat(savedItem.getPrice()).isEqualTo(item.getPrice()),
+                    () -> assertThat(savedItem.getImage()).isEqualTo(item.getImage()),
+                    () -> assertThat(savedItem.getDescription()).isEqualTo(item.getDescription())
+            );
+        }
+    }
+}
