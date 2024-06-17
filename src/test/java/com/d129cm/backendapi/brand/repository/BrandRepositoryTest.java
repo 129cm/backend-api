7aa680fd2fd79efe8468cc8e6af6c529e8d96989
kim-youngchul <kmosh1212@naver.com>
2024-06-17 16:19:08 +0900

    test: 브랜드 생성 기능 테스트 작성

--- src/test/java/com/d129cm/backendapi/brand/repository/BrandRepositoryTest.java
+++ src/test/java/com/d129cm/backendapi/brand/repository/BrandRepositoryTest.java
@@ -2,6 +2,7 @@ package com.d129cm.backendapi.brand.repository;
 
 import com.d129cm.backendapi.brand.domain.Brand;
 import com.d129cm.backendapi.config.InitializeTestContainers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -18,6 +19,17 @@ public class BrandRepositoryTest {
     @Autowired
     private BrandRepository brandRepository;
 
+    @BeforeEach
+    void init() {
+        Brand testBrand = Brand.builder()
+                .name("testBrand")
+                .description("testDescription")
+                .image("testImage")
+                .build();
+
+        brandRepository.save(testBrand);
+    }
+
     @Test
     void 성공_브랜드_저장() {
         // given
@@ -33,6 +45,17 @@ public class BrandRepositoryTest {
         // then
         assertThat(savedBrand).isNotNull();
         assertThat(savedBrand.getId()).isEqualTo(brand.getId());
+    }
 
+    @Test
+    void true_브랜드_이름으로_존재유무_확인() {
+        // given
+        String brandName = "testBrand";
+
+        // when
+        boolean isExist = brandRepository.existsByName(brandName);
+
+        // then
+        assertThat(isExist).isTrue();
     }
 }
