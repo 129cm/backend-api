d706b6ffc52443b82839f74e4d5257610ef10c76
kuma0112 <lsj23871@gmail.com>
2024-07-14 00:25:49 +0900

    feat: item, itemOption manager 생성

---
+++ src/main/java/com/d129cm/backendapi/cart/manager/ItemCartManager.java
@@ -0,0 +1,19 @@
+package com.d129cm.backendapi.cart.manager;
+
+import com.d129cm.backendapi.cart.domain.ItemCart;
+import com.d129cm.backendapi.cart.repository.ItemCartRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+@Transactional
+public class ItemCartManager {
+
+    private final ItemCartRepository itemCartRepository;
+
+    public void createCartItem(ItemCart itemCart) {
+        itemCartRepository.save(itemCart);
+    }
+}
