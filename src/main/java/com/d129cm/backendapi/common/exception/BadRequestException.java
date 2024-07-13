4ff87163b3d045a727b948ae16eddd47478422a5
kuma0112 <lsj23871@gmail.com>
2024-07-14 00:32:30 +0900

    feat: 장바구니에 아이템 추가 기능 생성

--- src/main/java/com/d129cm/backendapi/common/exception/BadRequestException.java
+++ src/main/java/com/d129cm/backendapi/common/exception/BadRequestException.java
@@ -3,6 +3,10 @@ package com.d129cm.backendapi.common.exception;
 import org.springframework.http.HttpStatus;
 
 public class BadRequestException extends BaseException {
+    private static final String EXCEED_LIMIT = "적용할 수 있는 수량 %s(을)를 초과하였습니다";
+    private static final String NEGATIVE_QUANTITY = "수량은 0 이하일 수 없습니다. 최소 1 이상의 값을 입력해 주세요.";
+
+
     private BadRequestException(String message) {
         super(message, HttpStatus.BAD_REQUEST);
     }
@@ -11,5 +15,12 @@ public class BadRequestException extends BaseException {
         String stringFormat = String.format("이미 %s 연관관계가 존재합니다.", fieldName);
 
         return new BadRequestException(stringFormat);
+    }
+    public static BadRequestException exceedQuantityLimit(int number) {
+        return new BadRequestException(String.format(EXCEED_LIMIT, number));
+    }
+
+    public static BadRequestException negativeQuantityLimit() {
+        return new BadRequestException(String.format(NEGATIVE_QUANTITY));
     }
 }
