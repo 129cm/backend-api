9d0c5db221e332691686a79905c96b788360aa78
kim-youngchul <kmosh1212@naver.com>
2024-07-15 23:58:32 +0900

    feat: 파트너스 아이템 조회 repository 기능 작성

--- src/main/java/com/d129cm/backendapi/item/repository/ItemRepository.java
+++ src/main/java/com/d129cm/backendapi/item/repository/ItemRepository.java
@@ -4,7 +4,13 @@ import com.d129cm.backendapi.item.domain.Item;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
     Page<Item> findAllByBrandId(Long brandId, Pageable pageable);
+
+    @Query(value = "select i from Item i where i.brand.id in (select b.id from Brand b join Partners p on p.brand.id = b.id where p.id = :partnersId)",
+            countQuery = "select count(i) from Item i where i.brand.id in (select b.id from Brand b join Partners p on p.brand.id = b.id where p.id = :partnersId)")
+    Page<Item> findAllByPartnersId(@Param("partnersId") Long partnersId, Pageable pageable);
 }
