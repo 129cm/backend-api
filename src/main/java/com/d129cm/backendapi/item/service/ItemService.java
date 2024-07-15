a2c77a133621fc0e86016fa94d747863e1db8e21
kim-youngchul <kmosh1212@naver.com>
2024-07-16 00:40:36 +0900

    feat: 파트너스 아이템 조회 itemService 기능 작성 및 테스트

--- src/main/java/com/d129cm/backendapi/item/service/ItemService.java
+++ src/main/java/com/d129cm/backendapi/item/service/ItemService.java
@@ -4,10 +4,16 @@ import com.d129cm.backendapi.brand.domain.Brand;
 import com.d129cm.backendapi.brand.manager.BrandManager;
 import com.d129cm.backendapi.item.domain.Item;
 import com.d129cm.backendapi.item.domain.ItemOption;
+import com.d129cm.backendapi.item.domain.SortCondition;
 import com.d129cm.backendapi.item.dto.ItemCreateRequest;
 import com.d129cm.backendapi.item.manager.ItemManager;
 import com.d129cm.backendapi.partners.domain.Partners;
+import com.d129cm.backendapi.partners.dto.GetItemsForPartnersResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,5 +36,16 @@ public class ItemService {
 
         brand.addItem(newItem);
         itemManager.createItem(newItem);
+    }
+
+    public GetItemsForPartnersResponse getItemsForPartners(
+            Partners partners, SortCondition sortCondition, Sort.Direction sortOrder, int page) {
+
+        int size = 50;
+        Sort sortObj = itemManager.createItemSort(sortCondition, sortOrder);
+        Pageable pageable = PageRequest.of(page, size, sortObj);
+        Page<Item> items = itemManager.getAllItemByPartnersId(partners.getId(), pageable);
+
+        return GetItemsForPartnersResponse.of(items);
     }
 }
