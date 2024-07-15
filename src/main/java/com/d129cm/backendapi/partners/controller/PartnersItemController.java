865bfd6bef675e03bbbefa33e61c3eb6b97eec24
kim-youngchul <kmosh1212@naver.com>
2024-07-16 00:41:22 +0900

    feat: 파트너스 아이템 조회 PartnersItemController 기능작성 및 테스트

--- src/main/java/com/d129cm/backendapi/partners/controller/PartnersItemController.java
+++ src/main/java/com/d129cm/backendapi/partners/controller/PartnersItemController.java
@@ -1,15 +1,16 @@
 package com.d129cm.backendapi.partners.controller;
 
 import com.d129cm.backendapi.common.dto.CommonResponse;
+import com.d129cm.backendapi.item.domain.SortCondition;
 import com.d129cm.backendapi.item.dto.ItemCreateRequest;
 import com.d129cm.backendapi.item.service.ItemService;
 import com.d129cm.backendapi.partners.domain.Partners;
+import com.d129cm.backendapi.partners.dto.GetItemsForPartnersResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,5 +25,16 @@ public class PartnersItemController {
 
         itemService.createItem(partners, request);
         return ResponseEntity.ok(CommonResponse.success());
+    }
+
+    @GetMapping("/partners/brands/items")
+    public ResponseEntity<CommonResponse<?>> getItemForPartners(
+            @AuthenticationPrincipal(expression = "partners") Partners partners,
+            @RequestParam(required = false, defaultValue = "NEW") SortCondition sort,
+            @RequestParam(required = false, defaultValue = "DESC") Sort.Direction sortOrder,
+            @RequestParam(required = false, defaultValue = "0") int page
+    ) {
+        GetItemsForPartnersResponse response = itemService.getItemsForPartners(partners, sort, sortOrder, page);
+        return ResponseEntity.ok(CommonResponse.success(response));
     }
 }
--- src/test/java/com/d129cm/backendapi/partners/controller/PartnersItemControllerTest.java
+++ src/test/java/com/d129cm/backendapi/partners/controller/PartnersItemControllerTest.java
@@ -3,10 +3,13 @@ package com.d129cm.backendapi.partners.controller;
 import com.d129cm.backendapi.auth.domain.PartnersDetails;
 import com.d129cm.backendapi.common.domain.Password;
 import com.d129cm.backendapi.config.TestSecurityConfig;
+import com.d129cm.backendapi.item.domain.SortCondition;
 import com.d129cm.backendapi.item.dto.ItemCreateRequest;
 import com.d129cm.backendapi.item.dto.ItemOptionCreateRequest;
 import com.d129cm.backendapi.item.service.ItemService;
 import com.d129cm.backendapi.partners.domain.Partners;
+import com.d129cm.backendapi.partners.dto.GetItemsForPartnersResponse;
+import com.d129cm.backendapi.partners.dto.ItemForPartnersResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -14,10 +17,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.nio.charset.StandardCharsets;
 import java.util.List;
@@ -67,6 +72,44 @@ public class PartnersItemControllerTest {
             result.andExpect(status().isOk())
                     .andExpect(jsonPath("$.status").value(200))
                     .andExpect(jsonPath("$.message").value("성공"));
+        }
+    }
+
+    @Nested
+    class GetItemsForPartnersTest {
+
+        @Test
+        void 성공200_브랜드_아이템_조회() throws Exception {
+            // given
+            Password password = mock(Password.class);
+            Partners mockPartners = spy(Partners.builder()
+                    .email("testPartners@email.com")
+                    .password(password))
+                    .businessNumber("123-45-67890")
+                    .build();
+            when(password.getPassword()).thenReturn("asdf123!");
+            GetItemsForPartnersResponse response = new GetItemsForPartnersResponse(List.of(
+                    new ItemForPartnersResponse(1L, "Item 1", 1000, "image1.jpg", "Brand A", null),
+                    new ItemForPartnersResponse(2L, "Item 2", 2000, "image2.jpg", "Brand A", null)
+            ));
+
+            when(itemService.getItemsForPartners(any(Partners.class), any(SortCondition.class), any(Sort.Direction.class), anyInt())).thenReturn(response);
+
+            // when
+            ResultActions result = mockMvc.perform(MockMvcRequestBuilders.get("/partners/brands/items")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .characterEncoding(StandardCharsets.UTF_8)
+                    .param("sort", "NEW")
+                    .param("sortOrder", "DESC")
+                    .param("page", "0")
+                    .with(SecurityMockMvcRequestPostProcessors.user(spy(new PartnersDetails(mockPartners)))));
+
+            // then
+            result.andExpect(status().isOk())
+                    .andExpect(jsonPath("$.status").value(200))
+                    .andExpect(jsonPath("$.message").value("성공"))
+                    .andExpect(jsonPath("$.data.itemResponses[0].itemName").value("Item 1"))
+                    .andExpect(jsonPath("$.data.itemResponses[1].itemName").value("Item 2"));
         }
     }
 
