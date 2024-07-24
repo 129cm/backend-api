dbd4dc4e5501cfe13faae8f48202bad8dd3be710
kim-youngchul <kmosh1212@naver.com>
2024-07-24 21:12:10 +0900

    feat: 파트너스 아이템 상세조회 기능 구현 PartnersItemController 작성

--- src/main/java/com/d129cm/backendapi/partners/controller/PartnersItemController.java
+++ src/main/java/com/d129cm/backendapi/partners/controller/PartnersItemController.java
@@ -5,7 +5,9 @@ import com.d129cm.backendapi.item.domain.SortCondition;
 import com.d129cm.backendapi.item.dto.ItemCreateRequest;
 import com.d129cm.backendapi.item.service.ItemService;
 import com.d129cm.backendapi.partners.domain.Partners;
+import com.d129cm.backendapi.partners.dto.GetItemDetailsResponse;
 import com.d129cm.backendapi.partners.dto.GetItemsForPartnersResponse;
+import com.d129cm.backendapi.partners.service.PartnersItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 public class PartnersItemController {
 
     private final ItemService itemService;
+    private final PartnersItemService partnersItemService;
 
     @PostMapping("/partners/brands/items")
     public ResponseEntity<CommonResponse<?>> createItem(
@@ -36,5 +39,15 @@ public class PartnersItemController {
     ) {
         GetItemsForPartnersResponse response = itemService.getItemsForPartners(partners, sort, sortOrder, page);
         return ResponseEntity.ok(CommonResponse.success(response));
+    }
+
+    @GetMapping("/partners/brands/items/{itemId}")
+    public ResponseEntity<CommonResponse<?>> getItemDetailsForPartners(
+            @AuthenticationPrincipal(expression = "partners") Partners partners,
+            @PathVariable Long itemId
+    ) {
+        GetItemDetailsResponse response = partnersItemService.getItemDetails(itemId, partners.getId());
+        CommonResponse<GetItemDetailsResponse> success = CommonResponse.success(response);
+        return ResponseEntity.ok(success);
     }
 }
