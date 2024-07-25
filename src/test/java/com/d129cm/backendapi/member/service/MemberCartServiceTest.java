ba8ac9441c20474e3a737c054b4e3e693163b306
kuma0112 <lsj23871@gmail.com>
2024-07-26 00:26:10 +0900

    test: 멤버 장바구니 조회 기능 테스트 코드 추가

--- src/test/java/com/d129cm/backendapi/member/service/MemberCartServiceTest.java
+++ src/test/java/com/d129cm/backendapi/member/service/MemberCartServiceTest.java
@@ -1,14 +1,19 @@
 package com.d129cm.backendapi.member.service;
 
+import com.d129cm.backendapi.brand.domain.Brand;
 import com.d129cm.backendapi.cart.domain.Cart;
 import com.d129cm.backendapi.cart.domain.ItemCart;
 import com.d129cm.backendapi.cart.manager.ItemCartManager;
+import com.d129cm.backendapi.common.domain.Address;
+import com.d129cm.backendapi.common.domain.Password;
 import com.d129cm.backendapi.common.exception.BadRequestException;
+import com.d129cm.backendapi.fixture.Fixture;
 import com.d129cm.backendapi.item.domain.Item;
 import com.d129cm.backendapi.item.domain.ItemOption;
 import com.d129cm.backendapi.item.manager.ItemManager;
 import com.d129cm.backendapi.item.manager.ItemOptionManager;
 import com.d129cm.backendapi.member.domain.Member;
+import com.d129cm.backendapi.member.dto.CartForMemberResponse;
 import com.d129cm.backendapi.member.dto.CartItemRequest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Nested;
@@ -18,10 +23,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.testcontainers.shaded.org.checkerframework.checker.units.qual.C;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @SuppressWarnings("NonAsciiCharacters")
@@ -38,6 +48,8 @@ public class MemberCartServiceTest {
 
     @Mock
     private ItemCartManager itemCartManager;
+
+    private Fixture fixture = new Fixture();
 
     @Nested
     class addItemToCart{
@@ -87,6 +99,37 @@ public class MemberCartServiceTest {
             BadRequestException e = BadRequestException.exceedQuantityLimit(100);
             Assertions.assertThatThrownBy(() -> memberCartService.addItemToCart(member, request))
                     .isInstanceOf(e.getClass()).hasMessage(e.getMessage());
+        }
+    }
+
+    @Nested
+    class getCart {
+
+        @Test
+        void CartForMemberResponse리스트반환_카트_조회() {
+            // given
+
+            Member member = fixture.createMember();
+            Cart cart = fixture.createCart(member);
+            Brand brand = fixture.createBrand();
+            Item item = fixture.createItem(brand);
+            ItemOption itemOption = fixture.createItemOption(item);
+            ItemCart itemCart1 = fixture.createItemCart(item, itemOption);
+            List<ItemCart> itemCarts = new ArrayList<>();
+            itemCarts.add(itemCart1);
+
+            when(itemCartManager.getItemCart(cart.getId())).thenReturn(itemCarts);
+
+            List<CartForMemberResponse> expectedResponses = itemCarts.stream()
+                    .map(CartForMemberResponse::of)
+                    .collect(Collectors.toList());
+
+            // when
+            List<CartForMemberResponse> responses = memberCartService.getCart(member);
+
+            // then
+            verify(itemCartManager).getItemCart(cart.getId());
+            assertThat(responses).isEqualTo(expectedResponses);
         }
     }
 }
