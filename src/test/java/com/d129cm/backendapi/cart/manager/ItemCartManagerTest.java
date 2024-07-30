d42a69c13fbec6ff32ef7d7ce68b23b6046c2b9e
kuma0112 <lsj23871@gmail.com>
2024-07-30 16:14:54 +0900

    test: 멤버 장바구니 수량 수정 기능 itemcart manager 관련 테스트코드 추가

--- src/test/java/com/d129cm/backendapi/cart/manager/ItemCartManagerTest.java
+++ src/test/java/com/d129cm/backendapi/cart/manager/ItemCartManagerTest.java
@@ -3,20 +3,25 @@ package com.d129cm.backendapi.cart.manager;
 import com.d129cm.backendapi.cart.domain.Cart;
 import com.d129cm.backendapi.cart.domain.ItemCart;
 import com.d129cm.backendapi.cart.repository.ItemCartRepository;
+import com.d129cm.backendapi.common.exception.NotFoundException;
 import com.d129cm.backendapi.item.domain.Item;
 import com.d129cm.backendapi.item.domain.ItemOption;
 import com.d129cm.backendapi.member.dto.CartItemRequest;
+import com.d129cm.backendapi.member.dto.CartItemUpdateRequest;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -75,7 +80,7 @@ public class ItemCartManagerTest {
             // given
             CartItemRequest request = new CartItemRequest(1L, 2L, 10);
             Long cartId = 3L;
-            ItemCart mockItemCart = mock(ItemCart.class);
+            Optional<ItemCart> mockItemCart = Optional.ofNullable(mock(ItemCart.class));
 
             when(itemCartRepository.findByItemIdAndItemOptionIdAndCartId(1L, 2L, 3L))
                     .thenReturn(mockItemCart);
@@ -84,7 +89,7 @@ public class ItemCartManagerTest {
             ItemCart result = itemCartManager.findItemCart(request, cartId);
 
             // then
-            assertThat(result).isEqualTo(mockItemCart);
+            assertThat(result).isEqualTo(mockItemCart.get());
             verify(itemCartRepository).findByItemIdAndItemOptionIdAndCartId(1L, 2L, 3L);
         }
     }
@@ -103,6 +108,42 @@ public class ItemCartManagerTest {
             // then
             verify(mockItemCart).increaseCount(additionalCount);
             verify(itemCartRepository).save(mockItemCart);
+        }
+    }
+
+    @Nested
+    class updateItemQuantityInCart{
+        @Test
+        void 성공_카트아이템_수량조정() {
+            // given
+            Cart cart = Mockito.mock(Cart.class);
+            ItemCart itemCart = Mockito.mock(ItemCart.class);
+            when(itemCartRepository.findByItemIdAndItemOptionIdAndCartId(1L, 2L, 3L)).thenReturn(Optional.ofNullable(itemCart));
+            when(cart.getId()).thenReturn(3L);
+            CartItemUpdateRequest request = new CartItemUpdateRequest(1L, 2L, 5);
+
+            // when
+            itemCartManager.updateItemQuantityInCart(cart, request);
+
+            // then
+            verify(itemCartRepository).findByItemIdAndItemOptionIdAndCartId(request.itemId(), request.itemOptionId(), cart.getId());
+            verify(itemCart).updateCount(request.count());
+        }
+
+        @Test
+        void NotFoundException_존재하자얺는_itemcart를_수정하려는_경우() {
+            // given
+            Cart cart = Mockito.mock(Cart.class);
+            when(cart.getId()).thenReturn(3L);
+            CartItemUpdateRequest request = new CartItemUpdateRequest(1L, 2L, 5);
+
+            when(itemCartRepository.findByItemIdAndItemOptionIdAndCartId(request.itemId(), request.itemOptionId(), cart.getId()))
+                    .thenReturn(Optional.empty());
+
+            // when & then
+            assertThrows(NotFoundException.class, () -> {
+                itemCartManager.updateItemQuantityInCart(cart, request);
+            });
         }
     }
 }
