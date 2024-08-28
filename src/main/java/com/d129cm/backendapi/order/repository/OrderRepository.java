eeb0b31c3fea312e3ceab868bb4668a3a4895ee2
kuma0112 <lsj23871@gmail.com>
2024-08-28 23:54:04 +0900

    feat: 주문 생성 OrderRepository 생성

---
+++ src/main/java/com/d129cm/backendapi/order/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.d129cm.backendapi.order.repository;
+
+import com.d129cm.backendapi.order.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
