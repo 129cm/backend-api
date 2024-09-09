d14b0d8177da6546eb2da87858b26da29064e52f
kuma0112 <lsj23871@gmail.com>
2024-09-10 00:20:32 +0900

    feat: 회원 주문 접수 OrderItemOptionRepository 생성

--- src/main/java/com/d129cm/backendapi/order/repository/OrderItemOptionRepository.java
+++ src/main/java/com/d129cm/backendapi/order/repository/OrderItemOptionRepository.java
@@ -3,5 +3,8 @@ package com.d129cm.backendapi.order.repository;
 import com.d129cm.backendapi.order.domain.OrderItemOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderItemOptionRepository extends JpaRepository<OrderItemOption, Long> {
+    List<OrderItemOption> findByOrderId(Long orderId);
 }
