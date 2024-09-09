992129cb12f0ff10db007694e8e71c556bf8a762
kuma0112 <lsj23871@gmail.com>
2024-09-10 00:20:20 +0900

    feat: 회원 주문 접수 OrderRepository 생성

--- src/main/java/com/d129cm/backendapi/order/repository/OrderRepository.java
+++ src/main/java/com/d129cm/backendapi/order/repository/OrderRepository.java
@@ -3,5 +3,10 @@ package com.d129cm.backendapi.order.repository;
 import com.d129cm.backendapi.order.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    Optional<Order> findByOrderSerial(String orderSerial);
+
+    boolean existsByOrderSerial(String orderSerial);
 }
