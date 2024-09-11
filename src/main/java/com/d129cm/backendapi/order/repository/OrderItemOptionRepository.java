9fdd7d04be3c44d52be3d104f9bca0f8e72e5919
kuma0112 <lsj23871@gmail.com>
2024-09-11 23:31:55 +0900

    refactor: Long -> OrderItemOptionId 로 변경

--- src/main/java/com/d129cm/backendapi/order/repository/OrderItemOptionRepository.java
+++ src/main/java/com/d129cm/backendapi/order/repository/OrderItemOptionRepository.java
@@ -1,10 +1,11 @@
 package com.d129cm.backendapi.order.repository;
 
 import com.d129cm.backendapi.order.domain.OrderItemOption;
+import com.d129cm.backendapi.order.domain.OrderItemOptionId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface OrderItemOptionRepository extends JpaRepository<OrderItemOption, Long> {
+public interface OrderItemOptionRepository extends JpaRepository<OrderItemOption, OrderItemOptionId> {
     List<OrderItemOption> findByOrderId(Long orderId);
 }
