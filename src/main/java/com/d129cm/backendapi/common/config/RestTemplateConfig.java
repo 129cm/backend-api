aafad3c9ef1f4a460785548f78b9da1d84f27870
kuma0112 <lsj23871@gmail.com>
2024-09-16 13:11:19 +0900

    feat: RestTemplateConfig 생성

---
+++ src/main/java/com/d129cm/backendapi/common/config/RestTemplateConfig.java
@@ -0,0 +1,14 @@
+package com.d129cm.backendapi.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
