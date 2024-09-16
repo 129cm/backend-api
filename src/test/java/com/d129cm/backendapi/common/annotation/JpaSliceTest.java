022ac0c470e03e2d3f92df67ae645a7082e40aba
kim-youngchul <kmosh1212@naver.com>
2024-09-16 19:51:01 +0900

    refactor: JpaSliceTest QueryDslConfig import 추가

--- src/test/java/com/d129cm/backendapi/common/annotation/JpaSliceTest.java
+++ src/test/java/com/d129cm/backendapi/common/annotation/JpaSliceTest.java
@@ -1,9 +1,11 @@
 package com.d129cm.backendapi.common.annotation;
 
+import com.d129cm.backendapi.common.config.QueryDslConfig;
 import com.d129cm.backendapi.config.InitializeTestContainers;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.testcontainers.context.ImportTestcontainers;
+import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -15,5 +17,6 @@ import java.lang.annotation.Target;
 @DataJpaTest
 @ImportTestcontainers(InitializeTestContainers.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(QueryDslConfig.class)
 public @interface JpaSliceTest {
 }
