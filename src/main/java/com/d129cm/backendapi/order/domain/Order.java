5d8426edbdea8fa253e29bd64393b052c6c55468
kim-youngchul <kmosh1212@naver.com>
2024-09-18 02:02:01 +0900

    refactor: Order 의 commonCodeId 필드를 OrderItemOption 의 필드로 변경

--- src/main/java/com/d129cm/backendapi/order/domain/Order.java
+++ src/main/java/com/d129cm/backendapi/order/domain/Order.java
@@ -1,12 +1,11 @@
 package com.d129cm.backendapi.order.domain;
 
 import com.d129cm.backendapi.common.domain.BaseEntity;
-import com.d129cm.backendapi.common.domain.CommonCodeId;
-import com.d129cm.backendapi.common.domain.code.GroupName;
 import com.d129cm.backendapi.member.domain.Member;
 import jakarta.persistence.*;
-import lombok.*;
-import org.springframework.util.Assert;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
@@ -17,30 +16,20 @@ public class Order extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Embedded
-    @AttributeOverrides({
-            @AttributeOverride(name = "codeId", column = @Column(name = "code_id")),
-            @AttributeOverride(name = "groupId", column = @Column(name = "group_id"))
-    })
-    private CommonCodeId commonCodeId;
-
-    @Setter
     private String orderSerial;
 
-    @Setter
     private String payAuthKey;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Member member;
 
-    @Builder
-    private Order(CommonCodeId commonCodeId, Member member) {
-        Assert.isTrue(GroupName.주문.getGroupId().equals(commonCodeId.getGroupId()), "올바른 그룹 코드가 아닙니다.");
-        this.commonCodeId = commonCodeId;
+    public Order(Member member, String orderSerial) {
         this.member = member;
+        this.orderSerial = orderSerial;
     }
 
-    public void changeOrderState(CommonCodeId commonCodeId) {
-        this.commonCodeId = commonCodeId;
+    public void updatePayAuthKey(String payAuthKey) {
+        this.payAuthKey = payAuthKey;
     }
 }
+
--- src/main/java/com/d129cm/backendapi/order/domain/OrderItemOption.java
+++ src/main/java/com/d129cm/backendapi/order/domain/OrderItemOption.java
@@ -1,7 +1,10 @@
 package com.d129cm.backendapi.order.domain;
 
 import com.d129cm.backendapi.common.domain.BaseEntity;
+import com.d129cm.backendapi.common.domain.CommonCodeId;
+import com.d129cm.backendapi.common.domain.code.GroupName;
 import com.d129cm.backendapi.item.domain.ItemOption;
+import com.mysema.commons.lang.Assert;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -24,15 +27,31 @@ public class OrderItemOption extends BaseEntity {
     @MapsId("itemOptionId")
     private ItemOption itemOption;
 
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "codeId", column = @Column(name = "code_id")),
+            @AttributeOverride(name = "groupId", column = @Column(name = "group_id"))
+    })
+    private CommonCodeId commonCodeId;
+
     private Integer count;
+
     private Integer salesPrice;
 
     @Builder
-    private OrderItemOption(Order order, ItemOption itemOption, Integer count, Integer salesPrice) {
+    private OrderItemOption(Order order, ItemOption itemOption, CommonCodeId commonCodeId, Integer count, Integer salesPrice) {
+        Assert.isTrue(GroupName.주문.getGroupId().equals(commonCodeId.getGroupId()), "올바른 그룹 코드가 아닙니다.");
+
         this.id = new OrderItemOptionId(order.getId(), itemOption.getId());
         this.order = order;
         this.itemOption = itemOption;
+        this.commonCodeId = commonCodeId;
         this.count = count;
         this.salesPrice = salesPrice;
     }
+
+    public void changeOrderState(CommonCodeId commonCodeId) {
+        this.commonCodeId = commonCodeId;
+    }
 }
+
