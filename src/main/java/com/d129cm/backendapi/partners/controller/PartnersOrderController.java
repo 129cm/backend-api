75b4583b3fada1e3242c8c777d48dc8433e28fa2
kim-youngchul <kmosh1212@naver.com>
2024-09-18 00:42:57 +0900

    feat: 주문 상세 조회 기능 구현

--- src/main/java/com/d129cm/backendapi/partners/controller/PartnersOrderController.java
+++ src/main/java/com/d129cm/backendapi/partners/controller/PartnersOrderController.java
@@ -1,21 +1,21 @@
 package com.d129cm.backendapi.partners.controller;
 
 import com.d129cm.backendapi.common.dto.CommonResponse;
+import com.d129cm.backendapi.order.dto.OrderDetailsDto;
 import com.d129cm.backendapi.order.dto.OrdersSearchResultDto;
 import com.d129cm.backendapi.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/partners/brands/orders")
 public class PartnersOrderController {
 
     private final OrderService orderService;
 
-    @GetMapping("/partners/brands/orders")
+    @GetMapping
     public ResponseEntity<?> searchOrders(
             @RequestParam String itemName,
             @RequestParam String startDate,
@@ -24,6 +24,13 @@ public class PartnersOrderController {
             @RequestParam int size,
             @RequestParam int page) {
         OrdersSearchResultDto response = orderService.searchResult(itemName, startDate, endDate, orderState, size, page);
+        return ResponseEntity.ok(CommonResponse.success(response));
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<?> getOrderDetailsByOrderId(@PathVariable("orderId") Long orderId) {
+        OrderDetailsDto response = orderService.getOrderDetailsByOrderId(orderId);
+
         return ResponseEntity.ok(CommonResponse.success(response));
     }
 }
