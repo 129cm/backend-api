e6757d26c9b91803ec5819522a29072fe7efe544
kuma0112 <lsj23871@gmail.com>
2024-09-18 15:02:39 +0900

    refactor: (CommonCodeId 이동) OrderItemDto 리팩토링

--- src/main/java/com/d129cm/backendapi/order/dto/OrderItemDto.java
+++ src/main/java/com/d129cm/backendapi/order/dto/OrderItemDto.java
@@ -1,5 +1,7 @@
 package com.d129cm.backendapi.order.dto;
 
+import com.d129cm.backendapi.common.domain.code.CodeName;
+import com.d129cm.backendapi.common.domain.code.GroupName;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 import lombok.ToString;
@@ -12,13 +14,15 @@ public class OrderItemDto {
     private Long itemOptionId;
     private String itemOptionName;
     private Integer count;
+    private String orderState;
 
     @QueryProjection
-    public OrderItemDto(Integer count, String itemImage, String itemName, Long itemOptionId, String itemOptionName) {
+    public OrderItemDto(Integer count, String itemImage, String itemName, Long itemOptionId, String itemOptionName, String orderState) {
         this.count = count;
         this.itemImage = itemImage;
         this.itemName = itemName;
         this.itemOptionId = itemOptionId;
         this.itemOptionName = itemOptionName;
+        this.orderState = CodeName.from(orderState, GroupName.주문);
     }
 }
