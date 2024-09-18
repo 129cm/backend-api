61ddf22bfce158bb264ea53ebdf04115bd5da9e9
kuma0112 <lsj23871@gmail.com>
2024-09-18 15:04:23 +0900

    chore: unused import 삭제

--- src/test/java/com/d129cm/backendapi/order/repository/OrderRepositoryTest.java
+++ src/test/java/com/d129cm/backendapi/order/repository/OrderRepositoryTest.java
@@ -5,7 +5,6 @@ import com.d129cm.backendapi.common.config.JpaAuditingConfig;
 import com.d129cm.backendapi.member.domain.Member;
 import com.d129cm.backendapi.order.domain.Order;
 import com.d129cm.backendapi.order.dto.OrderDetailsDto;
-import com.d129cm.backendapi.order.dto.OrdersSearchResponseDto;
 import com.d129cm.backendapi.order.dto.OrdersSearchResultDto;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
