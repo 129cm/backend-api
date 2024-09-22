13b0cc67646978668353649a9d700ed375db1c27
kuma0112 <lsj23871@gmail.com>
2024-09-22 22:18:14 +0900

    feat: OrderAddressDto 생성자 추가

--- src/main/java/com/d129cm/backendapi/order/dto/OrderAddressDto.java
+++ src/main/java/com/d129cm/backendapi/order/dto/OrderAddressDto.java
@@ -1,5 +1,6 @@
 package com.d129cm.backendapi.order.dto;
 
+import com.d129cm.backendapi.member.domain.Member;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 
@@ -14,5 +15,11 @@ public class OrderAddressDto {
         this.addressDetails = addressDetails;
         this.roadNameAddress = roadNameAddress;
         this.zipCode = zipCode;
+    }
+
+    public OrderAddressDto(Member member) {
+        this.addressDetails = member.getAddress().getAddressDetails();
+        this.roadNameAddress = member.getAddress().getRoadNameAddress();
+        this.zipCode = member.getAddress().getZipCode();
     }
 }
