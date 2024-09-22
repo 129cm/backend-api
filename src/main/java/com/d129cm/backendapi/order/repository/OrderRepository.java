ab8113f99dc354b4e2cf08bd27034896ecc43214
kuma0112 <lsj23871@gmail.com>
2024-09-22 16:38:05 +0900

    feat: OrderRepository - findOrderByMemberId 생성

--- src/main/java/com/d129cm/backendapi/order/repository/OrderRepository.java
+++ src/main/java/com/d129cm/backendapi/order/repository/OrderRepository.java
@@ -1,7 +1,10 @@
 package com.d129cm.backendapi.order.repository;
 
 import com.d129cm.backendapi.order.domain.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -9,4 +12,10 @@ public interface OrderRepository extends JpaRepository<Order, Long>, OrderQueryR
     Optional<Order> findByOrderSerial(String orderSerial);
 
     boolean existsByOrderSerial(String orderSerial);
+
+    @Query (value = "select o from Order o " +
+            "join OrderItemOption oio " +
+            "on oio.order.id = o.id " +
+            "where o.member.id = :memberId")
+    Page<Order> findOrderByMemberId(Long memberId, Pageable pageable);
 }
